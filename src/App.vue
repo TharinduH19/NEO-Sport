8cd4a0ba29bea3219f24ce06c8c72ec977a75c9d
TharinduH19 <tharindu.hashan1219@gmail.com>
2026-04-12 15:36:18 +0530

    setup tail end CSS

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <h1 class="text-3xl font-bold text-blue-500">Tailwind Working</h1>
 </template>
